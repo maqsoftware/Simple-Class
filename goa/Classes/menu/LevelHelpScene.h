b122e8d1a699be39519d001fe20a86fa3fe43a4c
Vishal Maurya <vishalm@maqsoftware.com>
2019-06-28 14:18:54 +0530

    Add scroll in tutorial section of the software (#108)
    Fixes #71

--- goa/Classes/menu/LevelHelpScene.h
+++ goa/Classes/menu/LevelHelpScene.h
@@ -17,6 +17,7 @@ public:
     static cocos2d::Scene *createScene(std::string gameName);
     static LevelHelpScene *create(std::string gameName);
 	cocos2d::ui::Button *_resumeButton;
+    cocos2d::ui::ListView *listviewScroll;
     void onEnterTransitionDidFinish() override;
     void onExitTransitionDidStart() override;
 	void ResumeButtonAction(cocos2d::Ref *pSender, cocos2d::ui::Widget::TouchEventType eEventType);
