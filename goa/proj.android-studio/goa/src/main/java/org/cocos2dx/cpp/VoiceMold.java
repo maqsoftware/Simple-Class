6aa0fa5d0307ca831844bc342b9a3118b5f4a663
Bikash Tudu <tudu.bikashk@gmail.com>
2019-08-22 11:46:02 +0530

    Updated the TTS speedrate (#173)
    #172

--- goa/proj.android-studio/goa/src/main/java/org/cocos2dx/cpp/VoiceMold.java
+++ goa/proj.android-studio/goa/src/main/java/org/cocos2dx/cpp/VoiceMold.java
@@ -71,8 +71,8 @@ class VoiceMold {
         }
 
         String text = "";
-        wrapper.getTts().setPitch(1.0f);
-        wrapper.getTts().setSpeechRate(0.5f);
+        wrapper.getTts().setPitch(1.3f);
+        wrapper.getTts().setSpeechRate(0.7f);
         wrapper.getTts().speak(text, TextToSpeech.QUEUE_FLUSH, createParamsForSpeak());
     }
 
